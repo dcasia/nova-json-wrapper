5022396a58200a9cbe9beb27f0c09a2d668f7b67
Menno Tempelaar <menno@fourdesign.nl>
2020-06-09 13:31:27 +0200

    changed getOriginal to getRawOriginal

--- src/JsonWrapper.php
+++ src/JsonWrapper.php
@@ -101,7 +101,7 @@ class JsonWrapper extends Field
 
         if ($model->exists) {
 
-            $originalValues = json_decode($model->getOriginal($this->attribute), true) ?? [];
+            $originalValues = json_decode($model->getRawOriginal($this->attribute), true) ?? [];
 
             $clone->setRawAttributes(
                 collect($originalValues)->only($this->fields->map->attribute->filter())->toArray()
